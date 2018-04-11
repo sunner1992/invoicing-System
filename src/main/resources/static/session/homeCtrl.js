8969791ac13aef28946a9be88fc1a2dc4437d761
sun_jiamin <sun_jiamin@cdv.com>
2018-04-11 20:32:28 +0800

    主页的基本样式结构

--- src/main/resources/static/session/homeCtrl.js
+++ src/main/resources/static/session/homeCtrl.js
@@ -4,11 +4,11 @@ define(function (require) {
     // dynamic load services here or add into dependencies of ui-router state config
     // require('../services/usersService');
 
-    app.controller('homeCtrl', ['$scope', '$ngConfirm', function ($scope, $ngConfirm) {
+    app.controller('homeCtrl', ['$scope', '$ngConfirm', '$css', function ($scope, $ngConfirm, $css) {
         // shortcut to get angular injected service.
 //        var userServices = app.get('usersService');
 //        $scope.userList = usersService.list();
-	
+		$css.add('session/home.css');
 
     }]);
 
